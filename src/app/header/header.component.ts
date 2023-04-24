3655796c935466883e6b34491efbffebd25ba236
Marc <marc@aguardio.com>
2023-04-24 13:09:05 +0200

    Initializes entire app

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {}
+
+
